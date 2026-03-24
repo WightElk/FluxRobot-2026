9e6465d82e33101d434c225badcb3b90a1a5f8cd
Victor <vic.gorlov@gmail.com>
2026-03-24 16:43:21 -0400

    work 1

--- src/main/java/frc/robot/commands/ShootToHubCmd.java
+++ src/main/java/frc/robot/commands/ShootToHubCmd.java
@@ -1,10 +1,25 @@
 package frc.robot.commands;
 
 import java.util.function.Supplier;
+import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Measure.*;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Constants.IndexerConstants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.RangeTable;
+import frc.robot.Robot;
+import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.PositionMech;
 import frc.robot.subsystems.VelocityMech;
@@ -18,10 +33,26 @@ public class ShootToHubCmd extends Command
     private final VelocityMech indexer;
     private final CommandSwerveDrivetrain drivetrain;
     private final Supplier<Pose2d> poseProvider;
+    private final RangeTable rangeTable;
 
     private Pose2d currentPose;
+    private boolean running = false;
+    private boolean poseChanged = false;
+    private double positionTolerance = ShooterConstants.RangePositionTolerance;
+    private Translation2d blueHubPos = new Translation2d(Units.inchesToMeters(82.11), Units.inchesToMeters(158.84));
+    private Translation2d redHubPos = new Translation2d(Units.inchesToMeters(651.22 - 182.11), Units.inchesToMeters(158.84));
 
-    public ShootToHubCmd(VelocityMech2 shooter, PositionMech hood, VelocityMech feeder, VelocityMech indexer, CommandSwerveDrivetrain drivetrain, Supplier<Pose2d> poseProvider)
+    private double MaxSpeed = Constants.MaxSpeedCoef * TunerConstants.kSpeedAt12Volts.in(Meters.per(Second)); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(Radians.per(Second)); // 3/4 of a rotation per second max angular velocity
+
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+        .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage) // Use open-loop control for drive motors
+        .withForwardPerspective(ForwardPerspectiveValue.OperatorPerspective);  //OperatorPerspective
+
+    protected final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+
+    public ShootToHubCmd(VelocityMech2 shooter, PositionMech hood, VelocityMech feeder, VelocityMech indexer, CommandSwerveDrivetrain drivetrain, RangeTable rangeTable, Supplier<Pose2d> poseProvider)
     {
         this.shooter = shooter;
         this.hood = hood;
@@ -29,6 +60,7 @@ public class ShootToHubCmd extends Command
         this.indexer = indexer;
         this.drivetrain = drivetrain;
         this.poseProvider = poseProvider;
+        this.rangeTable = rangeTable;
         currentPose = poseProvider.get();
         //shooter.setTargetSpeed(speed);
         // Use addRequirements() here to declare subsystem dependencies.
@@ -48,53 +80,46 @@ public class ShootToHubCmd extends Command
         // Shooter to distance
         // Drivetrain to yaw
 
+        Pose2d pose = poseProvider.get();
+        Translation2d pos = pose.getTranslation();
+        Rotation2d heading = pose.getRotation();
+        double delta = pos.getDistance(currentPose.getTranslation());
+
+        if (delta >= positionTolerance)
+        {
+            Translation2d hubPos = Robot.isBlueSide() ? blueHubPos : redHubPos;
+            double distance = pos.getDistance(hubPos);
+
+            RangeTable.Range range = rangeTable.getRange(distance);
+            double speed = range.speed;
+            double hoodPos = range.elevation;
+
+            shooter.setSpeed(speed);
+            hood.run(hoodPos);
+
+            double angle = heading.getRadians();
+            double kP = 1;
+            double turn = -1.0 * angle * kP * MaxAngularRate;
+            drivetrain.setControl(drive.withVelocityX(0).withVelocityY(0)
+                .withRotationalRate(turn));
+            currentPose = pose;
+            poseChanged = true;
+        }
+
+        boolean atTargetAngle = false;
         // Wait until at setpoint
         // Shoot
-/*
-        if (shooter.atTarget() && drivetrain.atTargetAngle())
+        if (shooter.atTarget() && atTargetAngle)
         {
-            indexer`.shoot();
+            running = true;
+            poseChanged = false;
+            indexer.setTargetSpeed(-IndexerConstants.Speed);
+            indexer.setSpeed(-IndexerConstants.Speed);
+            feeder.setTargetSpeed(-IndexerConstants.FeederSpeed);
+            feeder.setSpeed(-IndexerConstants.FeederSpeed);
+            drivetrain.applyRequest(() -> brake);
         }
-
-        // Calculate drivetrain commands from Joystick values
-        double forward = -controller.getLeftY() * Constants.Swerve.kMaxLinearSpeed;
-        double strafe = -controller.getLeftX() * Constants.Swerve.kMaxLinearSpeed;
-        double turn = -controller.getRightX() * Constants.Swerve.kMaxAngularSpeed;
-
-        // Read in relevant data from the Camera
-        boolean targetVisible = false;
-        double targetYaw = 0.0;
-        var results = camera.getAllUnreadResults();
-        if (!results.isEmpty()) {
-            // Camera processed a new frame since last
-            // Get the last one in the list.
-            var result = results.get(results.size() - 1);
-            if (result.hasTargets()) {
-                // At least one AprilTag was seen by the camera
-                for (var target : result.getTargets()) {
-                    if (target.getFiducialId() == 7) {
-                        // Found Tag 7, record its information
-                        targetYaw = target.getYaw();
-                        targetVisible = true;
-                    }
-                }
-            }
-        }
-
-        // Auto-align when requested
-        if (controller.getAButton() && targetVisible) {
-            // Driver wants auto-alignment to tag 7
-            // And, tag 7 is in sight, so we can turn toward it.
-            // Override the driver's turn command with an automatic one that turns toward the tag.
-            turn = -1.0 * targetYaw * VISION_TURN_kP * Constants.Swerve.kMaxAngularSpeed;
-        }
-
-        // Command drivetrain motors based on target speeds
-        drivetrain.drive(forward, strafe, turn);
-
-        // Put debug information to the dashboard
-        SmartDashboard.putBoolean("Vision Target Visible", targetVisible);
-        */
+//        SmartDashboard.putBoolean("Vision Target Visible", targetVisible);
     }
 
     @Override
